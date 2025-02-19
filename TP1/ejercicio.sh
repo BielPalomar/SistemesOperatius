fc748ef69a5cc6fb8a62da8564df149d32efbf54
BinarySandia04 <aranseraroig@gmail.com>
2025-02-19 10:46:51 +0100

    Funciona TP1

--- TP1/ejercicio.sh
+++ TP1/ejercicio.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ $# -ne 1 ]; then
-  echo "El nmero de parámetros no es correcto"
+  echo "El número de parámetros no es correcto"
   exit 1
 fi
 file=$1
@@ -12,5 +12,12 @@ lines=$(cat $file)
 for line in $lines; do
   numLinies=${line#*;}
   nomFitxer=${line%;*}
-  echo "Primeres ${numLinies} del ${nomFitxer}"
+  if [ -f $nomFitxer ]; then
+    echo "Primeres ${numLinies} del ${nomFitxer}"
+    head -n $numLinies $nomFitxer
+    echo ""
+  else
+    echo "${nomFitxer} no existeix o es un directori"
+  fi
 done
+exit 0
